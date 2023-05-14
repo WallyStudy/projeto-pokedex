98b19689a2c33cc8e7005ab1ebf8cd0cd4ff7d6e
Wallysson Menezes <104404623+wallymv@users.noreply.github.com>
2023-05-14 18:50:58 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,3 @@
-//comentário de uma linha
-
-/* 
-    Escrever aqui o que precisa fazer na programação para não se perder, mas em projetos sérios, apagar essas cixas de comentários
-
-    - sempre fazer uma análise de objetivo e quebrar em passos para melhor funcionalidade
-*/
-
-// pegar objetivos e passos do bloco de texto e ir separando ao longo do corpo para não se perder em qual passo está
-
-//sempre criar nomes de variável específicos
 const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
 const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
